ef71b1e5069482cfa1cbe6fef31e7cfe0b66c450
Chawathe, Lalit <lalit.chawathe@changehealthcare.com>
2019-06-14 17:17:18 -0500

    Using Callable and using the output of it i.e. Future

---
+++ adv-concurrency-app/src/main/java/com/productiveAnalytics/concurrency/executors/FutureAndCallableExample.java
@@ -0,0 +1,40 @@
+package com.productiveAnalytics.concurrency.executors;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Using Callable and Future
+ * 
+ * @author lchawathe
+ */
+public class FutureAndCallableExample {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+        Callable<String> callable = () -> {
+            // Perform some computation
+            System.out.println("Entered    Callable at "+ System.nanoTime() +" nanoSecs");
+            TimeUnit.SECONDS.sleep(5);
+            return "Hello from Callable at "+ System.nanoTime() +" nanoSecs";
+        };
+
+        System.out.println("Submitting Callable");
+        Future<String> future = executorService.submit(callable);
+
+        // This line executes immediately
+        System.out.println("Do something else while callable is getting executed");
+
+        System.out.println("Retrieve the result of the future");
+        // Future.get() blocks until the result is available
+        String result = future.get();
+        System.out.println(result);
+
+        executorService.shutdown();
+    }
+
+}
