6f22844d27884c6350c3bbb39e9e562d91c39b06
Chawathe, Lalit <lalit.chawathe@changehealthcare.com>
2019-06-14 16:11:29 -0500

    Working example of Executor of Scheduled Thread pool

---
+++ adv-concurrency-app/src/main/java/com/productiveAnalytics/concurrency/executors/ScheduledExecutorExample.java
@@ -0,0 +1,19 @@
+package com.productiveAnalytics.concurrency.executors;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ScheduledExecutorExample {
+    public static void main(String[] args) {
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+        Runnable task = () -> {
+          System.out.println("Executing  task at " + System.nanoTime());
+        };
+
+        System.out.println("Submitting task at " + System.nanoTime() + " to be executed after 5 seconds.");
+        scheduledExecutorService.schedule(task, 5, TimeUnit.SECONDS);
+        
+        scheduledExecutorService.shutdown();
+    }
+}
